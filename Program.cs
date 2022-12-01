b98add21aee6d652b9e1cfaf93f838b0473a782c
Maria <112690332+mashakoloss@users.noreply.github.com>
2022-12-02 00:56:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_4_
+{
+    class User
+    {
+        public string login = "QWERTY";
+        public string name = "Denis";
+        public string surname = "Daninko";
+        public int age = 18;
+        public readonly DateTime date = DateTime.Now;
+
+
+        public User(string loginx, string namex, string surnamex, int agex)
+        {
+            loginx = login;
+            namex = name;
+            surnamex = surname;
+            agex = age;
+        }
+
+    }
+}
